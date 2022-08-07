e2c5f2c0f0bd30d264490151c4e4ad7fcd124bb7
tobias.wright <tobias.wright@outlook.com>
2022-08-07 13:03:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { TodoistApi } from '@doist/todoist-api-typescript'
+
+
+// Please supply API key
+const apiKey = "API key goes here";
+
+const api = new TodoistApi(apiKey);
+
+const Form = (props) => {
+  let [task, setTask] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const { id } = await api.addTask({ content: task })
+    const isClosed = await api.closeTask(id)
+    setTask('')
+    console.log(isClosed)
+  }
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 5 }}>
+        <Stack spacing={2}>
+          <Input
+            placeholder="task"
+            fullWidth
+            value={task}
+            onChange={event => setTask(event.target.value)}
+          />
+
+          <Button type="submit" variant="contained">I done it!</Button>
+        </Stack>
+      </Box>
+    </Box>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form />
     </div>
   );
 }
